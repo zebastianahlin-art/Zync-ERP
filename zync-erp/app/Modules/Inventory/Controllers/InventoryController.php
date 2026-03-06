44f2233507fedc563602e94153f7639dc4cb733c
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-06 11:41:57 +0100

    Update InventoryController.php

--- zync-erp/app/Modules/Inventory/Controllers/InventoryController.php
+++ zync-erp/app/Modules/Inventory/Controllers/InventoryController.php
@@ -1,13 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Modules\Inventory\Controllers;
 
 use App\Core\Controller;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class InventoryController extends Controller
 {
-    public function index()
+    public function index(Request $request, Response $response): Response
     {
-        return $this->view('Inventory/index');
+        return $this->view($response, 'Inventory/index', [
+            'pageTitle' => 'Inventory',
+        ]);
     }
 }
