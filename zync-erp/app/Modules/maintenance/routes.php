fb6bdddee318905c3a002250d39a4fed111f2ccb
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-08 01:01:19 +0100

    Update maintenance routes with new inventory actions
    Added new routes for material inventory actions and updated existing work order routes.

--- zync-erp/app/Modules/maintenance/routes.php
+++ zync-erp/app/Modules/maintenance/routes.php
@@ -5,22 +5,34 @@ use Modules\Maintenance\Controllers\PreventiveMaintenanceController;
 use Modules\Maintenance\Controllers\WorkOrderController;
 
 return [
-    ['GET',  '/maintenance',                             [MaintenanceDashboardController::class, 'index']],
+    // Dashboard
+    ['GET',  '/maintenance', [MaintenanceDashboardController::class, 'index']],
+    ['GET',  '/maintenance/dashboard', [MaintenanceDashboardController::class, 'index']],
 
-    ['GET',  '/maintenance/work-orders',                 [WorkOrderController::class, 'index']],
-    ['GET',  '/maintenance/work-orders/create',          [WorkOrderController::class, 'create']],
-    ['POST', '/maintenance/work-orders',                 [WorkOrderController::class, 'store']],
-    ['GET',  '/maintenance/work-orders/show',            [WorkOrderController::class, 'show']],
-    ['POST', '/maintenance/work-orders/status',          [WorkOrderController::class, 'updateStatus']],
-    ['POST', '/maintenance/work-orders/add-log',         [WorkOrderController::class, 'addLog']],
-    ['POST', '/maintenance/work-orders/add-material',    [WorkOrderController::class, 'addMaterial']],
+    // Work orders
+    ['GET',  '/maintenance/work-orders', [WorkOrderController::class, 'index']],
+    ['GET',  '/maintenance/work-orders/create', [WorkOrderController::class, 'create']],
+    ['POST', '/maintenance/work-orders/store', [WorkOrderController::class, 'store']],
+    ['GET',  '/maintenance/work-orders/show', [WorkOrderController::class, 'show']],
+    ['POST', '/maintenance/work-orders/update-status', [WorkOrderController::class, 'updateStatus']],
+    ['POST', '/maintenance/work-orders/add-log', [WorkOrderController::class, 'addLog']],
+
+    // Work order materials
+    ['POST', '/maintenance/work-orders/add-material', [WorkOrderController::class, 'addMaterial']],
     ['POST', '/maintenance/work-orders/update-material', [WorkOrderController::class, 'updateMaterial']],
     ['POST', '/maintenance/work-orders/delete-material', [WorkOrderController::class, 'deleteMaterial']],
 
-    ['GET',  '/maintenance/preventive',                  [PreventiveMaintenanceController::class, 'index']],
-    ['GET',  '/maintenance/preventive/create',           [PreventiveMaintenanceController::class, 'create']],
-    ['POST', '/maintenance/preventive',                  [PreventiveMaintenanceController::class, 'store']],
-    ['GET',  '/maintenance/preventive/show',             [PreventiveMaintenanceController::class, 'show']],
-    ['POST', '/maintenance/preventive/run-due',          [PreventiveMaintenanceController::class, 'runDueSchedules']],
-    ['POST', '/maintenance/preventive/complete-run',     [PreventiveMaintenanceController::class, 'completeRun']],
+    // New material inventory actions
+    ['POST', '/maintenance/work-orders/reserve-material', [WorkOrderController::class, 'reserveMaterial']],
+    ['POST', '/maintenance/work-orders/issue-material', [WorkOrderController::class, 'issueMaterial']],
+    ['POST', '/maintenance/work-orders/return-material', [WorkOrderController::class, 'returnMaterial']],
+
+    // Preventive maintenance
+    ['GET',  '/maintenance/preventive', [PreventiveMaintenanceController::class, 'index']],
+    ['GET',  '/maintenance/preventive/create', [PreventiveMaintenanceController::class, 'create']],
+    ['POST', '/maintenance/preventive/store', [PreventiveMaintenanceController::class, 'store']],
+    ['GET',  '/maintenance/preventive/show', [PreventiveMaintenanceController::class, 'show']],
+    ['POST', '/maintenance/preventive/update', [PreventiveMaintenanceController::class, 'update']],
+    ['POST', '/maintenance/preventive/delete', [PreventiveMaintenanceController::class, 'delete']],
+    ['POST', '/maintenance/preventive/run-now', [PreventiveMaintenanceController::class, 'runNow']],
 ];
