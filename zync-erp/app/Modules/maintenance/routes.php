30af6467e70e7f6cfbaf0fe9ab3bd37d853b029c
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-08 01:36:28 +0100

    Refactor maintenance routes to use closures

--- zync-erp/app/Modules/maintenance/routes.php
+++ zync-erp/app/Modules/maintenance/routes.php
@@ -1,38 +1,139 @@
 <?php
 
+declare(strict_types=1);
+
 use Modules\Maintenance\Controllers\MaintenanceDashboardController;
 use Modules\Maintenance\Controllers\PreventiveMaintenanceController;
 use Modules\Maintenance\Controllers\WorkOrderController;
 
-return [
+return function ($app): void {
     // Dashboard
-    ['GET',  '/maintenance', [MaintenanceDashboardController::class, 'index']],
-    ['GET',  '/maintenance/dashboard', [MaintenanceDashboardController::class, 'index']],
+    $app->get('/maintenance', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->index();
+        return $response;
+    });
+
+    $app->get('/maintenance/dashboard', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->index();
+        return $response;
+    });
 
     // Work orders
-    ['GET',  '/maintenance/work-orders', [WorkOrderController::class, 'index']],
-    ['GET',  '/maintenance/work-orders/create', [WorkOrderController::class, 'create']],
-    ['POST', '/maintenance/work-orders/store', [WorkOrderController::class, 'store']],
-    ['GET',  '/maintenance/work-orders/show', [WorkOrderController::class, 'show']],
-    ['POST', '/maintenance/work-orders/update-status', [WorkOrderController::class, 'updateStatus']],
-    ['POST', '/maintenance/work-orders/add-log', [WorkOrderController::class, 'addLog']],
+    $app->get('/maintenance/work-orders', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->index();
+        return $response;
+    });
 
-    // Work order materials
-    ['POST', '/maintenance/work-orders/add-material', [WorkOrderController::class, 'addMaterial']],
-    ['POST', '/maintenance/work-orders/update-material', [WorkOrderController::class, 'updateMaterial']],
-    ['POST', '/maintenance/work-orders/delete-material', [WorkOrderController::class, 'deleteMaterial']],
+    $app->get('/maintenance/work-orders/create', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->create();
+        return $response;
+    });
 
-    // New material inventory actions
-    ['POST', '/maintenance/work-orders/reserve-material', [WorkOrderController::class, 'reserveMaterial']],
-    ['POST', '/maintenance/work-orders/issue-material', [WorkOrderController::class, 'issueMaterial']],
-    ['POST', '/maintenance/work-orders/return-material', [WorkOrderController::class, 'returnMaterial']],
+    $app->post('/maintenance/work-orders/store', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->store();
+        return $response;
+    });
+
+    $app->get('/maintenance/work-orders/show', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->show();
+        return $response;
+    });
+
+    $app->post('/maintenance/work-orders/update-status', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->updateStatus();
+        return $response;
+    });
+
+    $app->post('/maintenance/work-orders/add-log', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->addLog();
+        return $response;
+    });
+
+    // Materials
+    $app->post('/maintenance/work-orders/add-material', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->addMaterial();
+        return $response;
+    });
+
+    $app->post('/maintenance/work-orders/update-material', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->updateMaterial();
+        return $response;
+    });
+
+    $app->post('/maintenance/work-orders/delete-material', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->deleteMaterial();
+        return $response;
+    });
+
+    $app->post('/maintenance/work-orders/reserve-material', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->reserveMaterial();
+        return $response;
+    });
+
+    $app->post('/maintenance/work-orders/issue-material', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->issueMaterial();
+        return $response;
+    });
+
+    $app->post('/maintenance/work-orders/return-material', function ($request, $response) {
+        $controller = $this->get(WorkOrderController::class);
+        $controller->returnMaterial();
+        return $response;
+    });
 
     // Preventive maintenance
-    ['GET',  '/maintenance/preventive', [PreventiveMaintenanceController::class, 'index']],
-    ['GET',  '/maintenance/preventive/create', [PreventiveMaintenanceController::class, 'create']],
-    ['POST', '/maintenance/preventive/store', [PreventiveMaintenanceController::class, 'store']],
-    ['GET',  '/maintenance/preventive/show', [PreventiveMaintenanceController::class, 'show']],
-    ['POST', '/maintenance/preventive/update', [PreventiveMaintenanceController::class, 'update']],
-    ['POST', '/maintenance/preventive/delete', [PreventiveMaintenanceController::class, 'delete']],
-    ['POST', '/maintenance/preventive/run-now', [PreventiveMaintenanceController::class, 'runNow']],
-];
+    $app->get('/maintenance/preventive', function ($request, $response) {
+        $controller = $this->get(PreventiveMaintenanceController::class);
+        $controller->index();
+        return $response;
+    });
+
+    $app->get('/maintenance/preventive/show', function ($request, $response) {
+        $controller = $this->get(PreventiveMaintenanceController::class);
+        $controller->show();
+        return $response;
+    });
+
+    $app->get('/maintenance/preventive/create', function ($request, $response) {
+        $controller = $this->get(PreventiveMaintenanceController::class);
+        $controller->create();
+        return $response;
+    });
+
+    $app->post('/maintenance/preventive/store', function ($request, $response) {
+        $controller = $this->get(PreventiveMaintenanceController::class);
+        $controller->store();
+        return $response;
+    });
+
+    $app->post('/maintenance/preventive/update', function ($request, $response) {
+        $controller = $this->get(PreventiveMaintenanceController::class);
+        $controller->update();
+        return $response;
+    });
+
+    $app->post('/maintenance/preventive/delete', function ($request, $response) {
+        $controller = $this->get(PreventiveMaintenanceController::class);
+        $controller->delete();
+        return $response;
+    });
+
+    $app->post('/maintenance/preventive/run-now', function ($request, $response) {
+        $controller = $this->get(PreventiveMaintenanceController::class);
+        $controller->runNow();
+        return $response;
+    });
+};
